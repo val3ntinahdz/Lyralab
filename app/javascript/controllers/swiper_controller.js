2eaaf748daf80ac7c2afab4de381f5baa8995aee
your_github_username <valentinaleonh8@gmail.com>
2024-10-09 18:21:23 -0600

    Add Style

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,41 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
-import Swiper from 'swiper/bundle';
+import { Controller } from "@hotwired/stimulus";
+import Swiper from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 
-// Connects to data-controller="swiper"
+// Conectar Swiper con Stimulus
 export default class extends Controller {
   connect() {
-    this.swiper = new Swiper(this.element, {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      loop: true,
-      pagination: {
+    console.log("Swiper is initializing...");
+
+    new Swiper('.swiper-container', {
+      slidesPerView: 1,          // Puedes ajustar esto para mostrar más slides a la vez
+      spaceBetween: 30,          // Espacio entre las tarjetas
+      loop: true,                // Desplazamiento infinito
+      grabCursor: true,          // Muestra un cursor "de agarre"
+      speed: 600,                // Velocidad de la transición (600ms)
+      pagination: {              // Agrega paginación (puntos de navegación)
         el: '.swiper-pagination',
         clickable: true,
       },
-
-      navigation: {
+      navigation: {              // Agrega botones de navegación
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 25,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 30,
-        },
-      },
     });
-  }
-
-  disconnect() {
-    this.swiper.destroy();
   }
 }
