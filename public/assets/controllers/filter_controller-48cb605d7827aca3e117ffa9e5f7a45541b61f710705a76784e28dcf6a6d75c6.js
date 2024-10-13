7d364f13e3c61e367984f05e5d731cd33a2377b3
your_github_username <valentinaleonh8@gmail.com>
2024-10-13 15:07:14 -0600

    Add precompiled assets

---
+++ public/assets/controllers/filter_controller-48cb605d7827aca3e117ffa9e5f7a45541b61f710705a76784e28dcf6a6d75c6.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="filter"
+export default class extends Controller {
+  static targets = ["tab", "content"]
+
+  connect() {
+    console.log("Add Dynamism to your filters!")
+  }
+};
---
+++ public/assets/controllers/scroll_controller-dbd65bc45aabad9bea6d2bc9cd024095d790fa89839ee80c9bcc784a165165c9.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["section"];
+
+  connect() {
+    this.initIntersectionObserver();
+  }
+
+  initIntersectionObserver() {
+    const options = {
+      threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    this.sectionTargets.forEach(section => {
+      observer.observe(section);
+    });
+  }
+};
